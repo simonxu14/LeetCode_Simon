0ec37bd04e16be7dfd23758394fe61a5b993d79d
Simon Xu <simonxu14@gmail.com>
2015-09-18 23:55:12 -0700

    89

---
+++ 89.Gray Code/Solution.py
@@ -0,0 +1,28 @@
+__author__ = 'Simon'
+class Solution(object):
+    def grayCode(self, n):
+        # result = []
+        # current = []
+        # current.append('0')
+        # current.append('1')
+        # for i in range(1, n):
+        #     result = []
+        #     for m in range(0, len(current)):
+        #         result.append('0' + current[m])
+        #     for n in range(0, len(current)):
+        #         result.append('1' + current[n])
+        # result = current
+        result = []
+        result.append(0)
+        j = 0
+        for i in range(0, n):
+            high = 1 << n
+            long = len(result)
+
+
+            j = long - 1
+            while j >= 0:
+                result.append(high + result[j])
+                print high + result[j]
+                j -= 1
+        return result
