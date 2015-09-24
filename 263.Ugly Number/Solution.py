d5fd625d981534aa54f8370696356cea978d7a19
Simon Xu <simonxu14@gmail.com>
2015-09-24 13:27:46 -0700

    263

---
+++ 263.Ugly Number/Solution.py
@@ -0,0 +1,21 @@
+__author__ = 'Simon'
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 1:
+            return True
+        if  num < 1:
+            return False
+        while num != 1:
+            if num % 2 == 0:
+                num = num/2
+            elif num % 3 == 0:
+                num = num/3
+            elif num % 5 == 0:
+                num = num/5
+            else:
+                return False
+        return True
