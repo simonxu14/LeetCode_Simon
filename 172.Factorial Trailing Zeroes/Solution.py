47cefa4a24d7d1fedb4296dd3b9774b641d21066
Simon Xu <simonxu14@gmail.com>
2015-10-05 13:45:18 -0700

    172

---
+++ 172.Factorial Trailing Zeroes/Solution.py
@@ -0,0 +1,13 @@
+__author__ = 'Simon'
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        x = 5
+        ans = 0
+        while n >= x:
+            ans += n/x
+            x *= 5
+        return ans
