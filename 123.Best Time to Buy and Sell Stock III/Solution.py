b0fb98f5796ac87ef0536805194619a808054cb8
Simon Xu <simonxu14@gmail.com>
2015-10-21 00:28:19 -0700

    123

---
+++ 123.Best Time to Buy and Sell Stock III/Solution.py
@@ -0,0 +1,116 @@
+__author__ = 'Simon'
+class Solution(object):
+    def maxProfit(self, prices):
+    #     """
+    #     :type prices: List[int]
+    #     :rtype: int
+    #     """
+    #     if len(prices) <=2:
+    #         return 0
+    #     result = 0
+    #     current = 0
+    #     for i in range(1, len(prices)):
+    #         current = self.computeMax(prices[:i]) + self.computeMax(prices[i:])
+    #         if current > result:
+    #             result = current
+    #     return result
+    #
+    # def computeMax(self, prices):
+    #     if len(prices) <= 1:
+    #         return 0
+    #     OPT = [0 for i in range(len(prices)+1)]
+    #     OPT[0] = 0
+    #     OPT[1] = 0
+    #     for i in range(2, len(prices)+1):
+    #         OPT[i] = max(0, OPT[i-1] - prices[i-2] + prices[i-1])
+    #     return max(OPT)
+
+
+
+
+
+
+
+
+
+
+
+        # if len(prices) <= 2:
+        #     return 0
+        # OPT = [0 for i in range(len(prices)+1)]
+        # OPT[0] = 0
+        # OPT[1] = 0
+        # for i in range(2, len(prices)+1):
+        #     OPT[i] = max(0, OPT[i-1] - prices[i-2] + prices[i-1])
+        # # print OPT
+        #
+        # prices2 = []
+        # for i in range(len(prices)):
+        #     prices2.append(prices[len(prices)-1-i])
+        #
+        # OPT2 = [0 for i in range(len(prices2)+1)]
+        # OPT2[0] = 0
+        # OPT2[1] = 0
+        # for i in range(2, len(prices2)+1):
+        #     OPT2[i] = min(0, OPT2[i-1] - prices2[i-2] + prices2[i-1])
+        # # print OPT2
+        # result = 0
+        # current = 0
+        # for i in range(2, len(prices)):
+        #     current = max(OPT[0:i+1]) + min(OPT2[0:len(prices)-i+1])*(-1)
+        #     if current > result:
+        #         result = current
+        # return result
+
+
+
+
+
+
+
+
+
+
+
+        # if len(prices) <= 2:
+        #     return 0
+        # OPT = [0 for i in range(len(prices)+1)]
+        # OPT[0] = 0
+        # OPT[1] = 0
+        # for i in range(2, len(prices)+1):
+        #     OPT[i] = max(0, OPT[i-1] - prices[i-2] + prices[i-1])
+        #
+        # result = 0
+        # current = 0
+        # for i in range(2, len(prices)-1):
+        #     current = max(OPT[:i+1]) + max(OPT[i+1:]) - min(OPT[i+1:])
+        #     if current > result:
+        #         result = current
+        # return result
+
+
+
+
+
+
+        profit = [0]*len(prices)
+        low, high = float('inf'), float('-inf')
+        max_profit = res = 0
+        for n, p in enumerate(prices):
+            low = min(p, low)
+            max_profit = max(max_profit, p-low)
+            profit[n] = max_profit
+        max_profit = 0
+        for n, p in reversed(list(enumerate(prices))):
+            high = max(p, high)
+            max_profit = max(max_profit, high-p)
+            res = max(res, max_profit+profit[n])
+        return res
+
+
+
+
+if __name__ == '__main__':
+    S = Solution()
+    prices = [1,2,3,4,1,2,3,4,1,2]
+    print S.maxProfit(prices)
