4665aa4e550f509942ea7c000bf8d71b2c0c1743
Simon Xu <simonxu14@gmail.com>
2015-10-21 01:53:31 -0700

    123update

--- 123.Best Time to Buy and Sell Stock III/Solution.py
+++ 123.Best Time to Buy and Sell Stock III/Solution.py
@@ -93,19 +93,72 @@ class Solution(object):
 
 
 
-        profit = [0]*len(prices)
-        low, high = float('inf'), float('-inf')
-        max_profit = res = 0
-        for n, p in enumerate(prices):
-            low = min(p, low)
-            max_profit = max(max_profit, p-low)
-            profit[n] = max_profit
-        max_profit = 0
-        for n, p in reversed(list(enumerate(prices))):
-            high = max(p, high)
-            max_profit = max(max_profit, high-p)
-            res = max(res, max_profit+profit[n])
-        return res
+        # profit = [0]*len(prices)
+        # low, high = float('inf'), float('-inf')
+        # max_profit = res = 0
+        # for n, p in enumerate(prices):
+        #     low = min(p, low)
+        #     max_profit = max(max_profit, p-low)
+        #     profit[n] = max_profit
+        # max_profit = 0
+        # for n, p in reversed(list(enumerate(prices))):
+        #     high = max(p, high)
+        #     max_profit = max(max_profit, high-p)
+        #     res = max(res, max_profit+profit[n])
+        # return res
+
+
+
+
+        if len(prices) < 2:
+            return 0
+        OPT = [0 for i in range(len(prices))]
+        OPT[0] = 0
+        highPrice = prices[0]
+        lowPrice = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] > highPrice:
+                highPrice = prices[i]
+                OPT[i] = highPrice - lowPrice
+            elif prices[i] < lowPrice:
+                OPT[i] = OPT[i-1]
+                lowPrice = prices[i]
+                highPrice = prices[i]
+            else:
+                OPT[i] = OPT[i-1]
+        print OPT
+
+
+
+        prices2 = []
+        for i in range(len(prices)):
+            prices2.append(prices[len(prices)-1-i])
+        # print prices2
+
+        result = 0
+
+        OPT2 = [0 for i in range(len(prices2))]
+        OPT2[0] = 0
+
+        highPrice = prices2[0]
+        lowPrice = prices2[0]
+
+        for i in range(1, len(prices2)):
+            if prices2[i] < lowPrice:
+                lowPrice = prices2[i]
+                OPT2[i] = highPrice - lowPrice
+            elif prices2[i] > highPrice:
+                OPT2[i] = OPT2[i-1]
+                highPrice = prices2[i]
+                lowPrice = prices2[i]
+            else:
+                OPT2[i] = OPT2[i-1]
+            if result < OPT2[i] + OPT[len(prices)-i-1]:
+                result = OPT2[i] + OPT[len(prices)-i-1]
+        print OPT2
+        return result
+
+
 
 
 
