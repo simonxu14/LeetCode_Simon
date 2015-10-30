12d8b78498ee763e6d2105c1e8399a1cd4d84688
Simon Xu <simonxu14@gmail.com>
2015-10-30 16:35:53 -0700

    77update

--- 77.Combinations/Solution.py
+++ 77.Combinations/Solution.py
@@ -6,21 +6,37 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        ans = []
-        stack = []
-        x = 1
-        while True:
-            l = len(stack)
-            if l == k:
-                ans.append(stack[:])
-            if l == k or x > n - k + l + 1:
-                if not stack:
-                    return ans
-                x = stack.pop() + 1
-            else:
-                stack.append(x)
-                x += 1
+        # ans = []
+        # stack = []
+        # x = 1
+        # while True:
+        #     l = len(stack)
+        #     if l == k:
+        #         ans.append(stack[:])
+        #     if l == k or x > n - k + l + 1:
+        #         if not stack:
+        #             return ans
+        #         x = stack.pop() + 1
+        #     else:
+        #         stack.append(x)
+        #         x += 1
+#
+# if __name__ == '__main__':
+#     S = Solution()
+#     print S.combine(4,2)
 
-if __name__ == '__main__':
-    S = Solution()
-    print S.combine(4,2)
+
+
+    def combine(self, n, k):
+        res = []
+        self.dfs(xrange(1,n+1), k, 0, [], res)
+        return res
+
+    def dfs(self, nums, k, index, path, res):
+        #if k < 0:  #backtracking
+            #return
+        if k == 0:
+            res.append(path)
+            return # backtracking
+        for i in xrange(index, len(nums)):
+            self.dfs(nums, k-1, i+1, path+[nums[i]], res)
